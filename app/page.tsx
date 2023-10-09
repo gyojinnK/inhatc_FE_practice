de396cc777e515345a618bcd0ef4c6c159ecaa65
gyojinnK <ksmug1998@gmail.com>
2023-10-10 02:33:21 +0900

    Wrap 컴포넌트 복수 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import css from "@/styles/page.module.css";
 import Wrap from "@/components/Wrap";
 
+const DATABASE1: any = [];
+const DATABASE2: any = [];
+const DATABASE3: any = [];
+
 export default function Home() {
     return (
         <>
-            <Wrap />
-            {/* <Wrap />
-            <Wrap /> */}
+            <Wrap grade={"1"} data={DATABASE1} />
+            <Wrap grade={"2"} data={DATABASE2} />
+            <Wrap grade={"3"} data={DATABASE3} />
         </>
     );
 }
