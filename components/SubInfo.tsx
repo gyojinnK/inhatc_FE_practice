8acd59cdd571c4126b114c04ba35a571c5ccfb2d
gyojinnK <ksmug1998@gmail.com>
2023-10-10 00:25:03 +0900

    수정, 추가, 분할 작업 진행

--- components/SubInfo.tsx
+++ components/SubInfo.tsx
@@ -1,24 +1,89 @@
 "use client";
 import css2 from "@/styles/SubInfo.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import LineCalc from "./LineCalc";
 
 const SubInfo = (props: any): any => {
+    const [select, setSelect] = useState(false);
+    const [total, setTotal] = useState(0);
+    // const [avg, setAvg] = useState(0.0);
+    const [score, setScore] = useState("");
+
+    useEffect(() => {
+        setTotal(
+            parseInt(props.attendance) +
+                parseInt(props.practice) +
+                parseInt(props.middle) +
+                parseInt(props.final)
+        );
+        setScore(() => {
+            let s = "";
+            if (total > 95) s = "A+";
+            else if (95 >= total && total > 90) s = "A0";
+            else if (90 >= total && total > 85) s = "B+";
+            else if (85 >= total && total > 80) s = "B0";
+            else if (80 >= total && total > 75) s = "C+";
+            else if (75 >= total && total > 70) s = "C0";
+            else if (70 >= total && total > 65) s = "D+";
+            else if (65 >= total && total > 60) s = "D0";
+            else s = "F";
+            return s;
+        });
+    }, [[props.idList]]);
+
+    const selectHandler = () => {
+        select ? setSelect(false) : setSelect(true);
+        props.onLifting(props.id);
+        console.log("SubINfo: " + props.id);
+    };
+
+    const optionalStyle = {
+        backgroundColor: select ? "rgba(0,0,0,0.8)" : "rgb(155, 155, 155)",
+    };
     return (
         <>
-            <tr>
-                <td className={css2.tdStyle}>{props.complete}</td>
-                <td className={css2.tdStyle}>{props.essential}</td>
-                <td className={`${css2.exeptThStyle} ${css2.tdStyle} `}>
+            <tr onClick={selectHandler}>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.complete}
+                </td>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.essential}
+                </td>
+                <td
+                    style={optionalStyle}
+                    className={`${css2.exeptThStyle} ${css2.tdStyle} `}
+                >
                     {props.sub_name}
                 </td>
-                <td className={css2.tdStyle}>{props.grade}</td>
-                <td className={css2.tdStyle}>{props.attendance}</td>
-                <td className={css2.tdStyle}>{props.practice}</td>
-                <td className={css2.tdStyle}>{props.middle}</td>
-                <td className={css2.tdStyle}>{props.final}</td>
-                <td className={css2.tdStyle}></td>
-                <td className={css2.tdStyle}></td>
-                <td className={css2.tdStyle}></td>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.grade}
+                </td>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.attendance}
+                </td>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.practice}
+                </td>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.middle}
+                </td>
+                <td style={optionalStyle} className={css2.tdStyle}>
+                    {props.final}
+                </td>
+
+                {props.grade == "1" ? (
+                    <LineCalc
+                        otionalStyle={optionalStyle}
+                        total={" "}
+                        score={"P"}
+                    />
+                ) : (
+                    <LineCalc
+                        otionalStyle={optionalStyle}
+                        total={total}
+                        score={score}
+                    />
+                )}
             </tr>
             <style jsx>
                 {`
